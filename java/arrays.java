117996886c46ff8136eeb57711c6216c0693c1da
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-04-23 14:59:01 -0400

    Update arrays.java

--- java/arrays.java
+++ java/arrays.java
@@ -1,37 +1,39 @@
-// Static Arrays
-
-String[] str_arr; // Initialize Array
-String[] str_arr = {"1", "2", "3", "4"}; // Change values
-int[] arr = {10, 20, 30, 40}; // Initialize array w/ values
-int[] arr = new int[2]; // Empty array - 0 is initialized at first
-
-// Modify
-str_arr[0] = "0";
-
-// Retrieve
-str_arr[0];
-
-// Dynamic array in java
-
-import java.util.ArrayList;
-ArrayList<Integer> list = new ArrayList<>();
-list.add(10);  // append
-
-// No slicing. Instead:
+import java.util.ArrayList; // Imports required to be on top of file
 import java.util.Arrays;
-int[] arr = {10, 20, 30, 40, 50};
-int[] newArr = Arrays.copyOfRange(arr, 1, 4); 
 
-// Get length of array
-int len = arr.length; // Since array is array object
-String s = "hello";
-int len = s.length(); // Since s is a string object
+public class Main {
+    public static void main(String[] args) {
 
-// 2D array
-int[][] grid = new int[3][4]; // 3 rows, 4 columns
-for (int i = 0; i < grid.length; i++) {
-    for (int j = 0; j < grid[i].length; j++) {
-        System.out.print(grid[i][j] + " ");
+        // Static Arrays
+        String[] str_arr = {"1", "2"};
+        str_arr[0] = "0";
+        String my_str = str_arr[0];
+
+        int[] arr1 = {10, 20};
+        int len1 = arr1.length;
+
+        int[] arr2 = new int[2]; // defaults to 0
+
+        String s = "hello";
+        int strLen = s.length();
+
+        // Dynamic array
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        int val = list.remove(list.size() - 1);
+
+        // Slicing
+        int[] arr3 = {10, 20, 30, 40, 50};
+        int[] arrNew = Arrays.copyOfRange(arr3, 1, 4);
+
+        // 2D array
+        int[][] grid = new int[3][4];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-    System.out.println();
 }
