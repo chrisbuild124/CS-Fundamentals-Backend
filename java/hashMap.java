8302c836e061f3eb3dfcf09650daba075af9a9a6
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-04-23 14:01:51 -0400

    Create hashMap.java

---
+++ java/hashMap.java
@@ -0,0 +1,8 @@
+// Java HashMaps
+
+Map<Integer, Integer> indices = new HashMap<>();
+indices.put(1, 2); // pushes key value pair
+indices.get(1); // Get value
+indices.containsKey(1); // Contains key boolean
+indices.remove(1); // Remove pair
+indices.clear(); // Clear entire hashmap
