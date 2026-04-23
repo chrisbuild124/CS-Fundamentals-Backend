f0b68a3745f7496ada12897372d6025706832410
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-04-23 13:28:47 -0400

    Update arrays.java

--- java/arrays.java
+++ java/arrays.java
@@ -33,3 +33,14 @@ int[] newArr = Arrays.copyOfRange(arr, 1, 4);
 int len = arr.length; // Since array is array object
 String s = "hello";
 int len = s.length(); // Since s is a string object
+
+// 2D array
+int[][] grid = new int[3][4]; // 3 rows, 4 columns
+// Accessing
+for (int i = 0; i < grid.length; i++) {
+    for (int j = 0; j < grid[i].length; j++) {
+        System.out.print(grid[i][j] + " ");
+    }
+    System.out.println();
+}
+
