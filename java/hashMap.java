06a8a3b08ed61313e9004c940fae7563ca1dec75
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-11 01:10:08 -0400

    Update hashMap.java

--- java/hashMap.java
+++ java/hashMap.java
@@ -1,12 +1,9 @@
 import java.util.Map;
 import java.util.HashMap;
 
-public class Main {
-    public static void main(String[] args) {
-      Map<Integer, Integer> indices = new HashMap<>();
-      indices.put(1, 2); // pushes key value pair
-      indices.get(1); // Get value
-      indices.containsKey(1); // Contains key boolean
-      indices.remove(1); // Remove pair
-      indices.clear(); // Clear entire hashmap
-}
+Map<Integer, Integer> indices = new HashMap<>();
+indices.put(1, 2); // pushes key value pair
+indices.get(1); // Get value
+indices.containsKey(1); // Contains key boolean
+indices.remove(1); // Remove pair
+indices.clear(); // Clear entire hashmap
