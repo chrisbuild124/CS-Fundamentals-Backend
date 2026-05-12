319373bd7c9143cc5fad94c8cc7dfe09416c0b13
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-12 17:42:18 -0400

    Update stream.java

--- java/stream.java
+++ java/stream.java
@@ -2,3 +2,11 @@ import java.util.Arrays;
 
 int[] arr = {1, 2, 3, 4}; // Stream allows chaining
 int sum = Arrays.stream(arr).sum();
+
+
+// Example
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        return Arrays.stream(nums).distinct().count() != nums.length;
+    }
+}
