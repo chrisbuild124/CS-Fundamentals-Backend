a5df5fade5a3c1f533ca3406a8967e6379055efc
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-12 16:35:51 -0400

    Update dynamicArrays.java

--- java/arrays/dynamicArrays.java
+++ java/arrays/dynamicArrays.java
@@ -6,4 +6,5 @@ list.add(10); // Add
 int val = list.remove(list.size() - 1); // Remove
 
 // Slicing
-String newString strs[0].substring(0, 1);
+String[] strs = {"hello"};
+String newString = strs[0].substring(0, 1);
