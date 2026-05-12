0ea60c065b11cb1da43a393f6ac651a995707423
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-12 00:40:31 -0400

    Create dynamicArrays.java

---
+++ java/arrays/dynamicArrays.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+// Dynamic array
+ArrayList<Integer> list = new ArrayList<>();
+list.add(10); // Add
+int val = list.remove(list.size() - 1); // Remove
+
+// Slicing
+String newString strs[0].substring(0, 1);
