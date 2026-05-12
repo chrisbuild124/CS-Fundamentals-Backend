87db1aad96005e4d5806d031b5684772055a1368
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-12 00:46:35 -0400

    Create misc.java

---
+++ java/arrays/misc.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+// Normal char array
+char[] str_arr = {"-1", "1"};
+str_arr[0] = "0";
+int len1 = str_arr.length;
+
+// Special string array
+String new_str = "12345";
+int len1 = new_str.length();
