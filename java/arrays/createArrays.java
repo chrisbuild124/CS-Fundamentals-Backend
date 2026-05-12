69e07c872423ae6bcc34d02548447266dcb25643
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-11 23:49:44 -0400

    Create createArrays.java

---
+++ java/arrays/createArrays.java
@@ -0,0 +1,10 @@
+// Create Arrays with inputs
+String[] str_arr = {"1", "2"};
+int[] arr1 = {10, 20};
+
+// Create arrays without inputs
+String[] str_arr = new String[2];
+int[] arr1 = new int[2];
+
+// Create 2D array
+int[][] grid = new int[3][4];
