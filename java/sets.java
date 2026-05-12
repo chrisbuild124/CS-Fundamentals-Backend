bcd9b32d3a211248e4a4cb802a08839117899bd6
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-12 16:47:46 -0400

    Update sets.java

--- java/sets.java
+++ java/sets.java
@@ -1,1 +1,14 @@
+import java.util.Set;
+import java.util.HashSet;
 
+Set<Integer> set1 = new HashSet<>(); // Usable to multiple DSA types
+set1.add(1);        // insert
+set1.remove(1);     // delete
+set1.contains(1);   // lookup
+set1.size();        // size
+
+HashSet<Integer> set2 = new HashSet<>();
+set2.add(1);        // insert
+set2.remove(1);     // delete
+set2.contains(1);   // lookup
+set2.size();        // size
