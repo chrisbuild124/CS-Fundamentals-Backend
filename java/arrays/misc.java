75ecfaa4c8503b036b1950453bcab7978222bf84
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-12 16:33:20 -0400

    Update misc.java

--- java/arrays/misc.java
+++ java/arrays/misc.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
 // Normal char array
-char[] str_arr = {"-1", "1"};
-str_arr[0] = "0";
+char[] str_arr = {'1', '1'};
+str_arr[0] = '0'; // must be '' for char; "" for string
 int len1 = str_arr.length;
 
 // Special string array
 String new_str = "12345";
-int len1 = new_str.length();
+int len2 = new_str.length();
