1fc4cd880f745ba15d9b48ab6a169beb7fe8a06e
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-16 16:05:59 -0400

    Create arrays.java

---
+++ java/math/arrays.java
@@ -0,0 +1,10 @@
+int minVal = Math.min(5, 3);
+int maxVal = Math.max(5, 3);
+int absVal = Math.abs(-5);
+
+// Doubles converted to ints
+double pwer = Math.pow(2, 3); // This one left as is
+int sq_val = (int) Math.sqrt(16);
+int ceil = (int) Math.ceil(102.6);
+int floor = (int) Math.floor(102.6);
+int rounded = (int) Math.round(102.6);
