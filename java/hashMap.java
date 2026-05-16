3554c5b186188548e2d0b20c0532117df16c399d
Chris Sexton <70672189+chrisbuild124@users.noreply.github.com>
2026-05-16 17:21:31 -0400

    Update hashMap.java

--- java/hashMap.java
+++ java/hashMap.java
@@ -1,5 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 HashMap<Integer, Integer> indices1 = new HashMap<>();
 indices1.put(1, 2);        // pushes key value pair
@@ -16,3 +18,8 @@ indices2.containsKey(1);   // Contains key boolean
 indices2.remove(1);        // Remove pair
 indices2.clear();          // Clear entire hashmap
 indices2.putIfAbsent(1, 1); // Add default value
+
+Map<String, List<String>> res = new HashMap<>(); // Return list of lists based on key
+res.putIfAbsent(key, new ArrayList<>());
+res.get(key).add(string);
+return new ArrayList<>(res.values());
